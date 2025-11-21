966fd26ddce8b8ad24ba5fc64964884f134b997b
andrefilipeneves <90059756+andrefilipeneves@users.noreply.github.com>
2025-11-21 11:07:57 +0000

    Fix string quotes and date_created default value

--- apps/models.py
+++ apps/models.py
@@ -3,27 +3,25 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from email.policy import default
 from apps import db
 from sqlalchemy.exc import SQLAlchemyError
 from apps.exceptions.exception import InvalidUsage
 import datetime as dt
-from sqlalchemy.orm import relationship
 from enum import Enum
 
 class CURRENCY_TYPE(Enum):
-    usd = 'usd'
-    eur = 'eur'
+    usd = "usd"
+    eur = "eur"
 
 class Product(db.Model):
-    __tablename__ = 'products'
+    __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     info = db.Column(db.Text, nullable=True)
     price = db.Column(db.Integer, nullable=False)
     currency = db.Column(db.Enum(CURRENCY_TYPE), default=CURRENCY_TYPE.usd, nullable=False)
-    date_created = db.Column(db.DateTime, default=dt.datetime.utcnow())
+    date_created = db.Column(db.DateTime, default=dt.datetime.utcnow)
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __init__(self, **kwargs):
@@ -47,7 +45,7 @@ class Product(db.Model):
         except SQLAlchemyError as e:
             db.session.rollback()
             db.session.close()
-            error = str(e.__dict__['orig'])
+            error = str(e.__dict__["orig"])
             raise InvalidUsage(error, 422)
 
     def delete(self) -> None:
@@ -57,20 +55,11 @@ class Product(db.Model):
         except SQLAlchemyError as e:
             db.session.rollback()
             db.session.close()
-            error = str(e.__dict__['orig'])
+            error = str(e.__dict__["orig"])
             raise InvalidUsage(error, 422)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=dt.datetime.utcnow)
-
 class ActivityLog(db.Model):
-    __tablename__ = 'activity_logs'
-    __table_args__ = {'extend_existing': True}
+    __tablename__ = "activity_logs"
 
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String(255), nullable=False)
